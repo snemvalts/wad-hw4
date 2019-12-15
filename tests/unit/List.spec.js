5c0fac9ac509ad14fa9b300ac400d7672f6cb897
Krister Looga <looga.krister@gmail.com>
2019-12-15 21:16:35 +0200

    Added list done test. Credits to RaulEK

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,5 +1,6 @@
 import {mount} from '@vue/test-utils'
 import List from "../../src/components/List";
+import flushPromises from "flush-promises";
 
 describe('List component', () => {
 
@@ -7,9 +8,24 @@ describe('List component', () => {
     propsData: {
       list: []
     }
-  })
+  });
 
   it('Empty list displays correct text', () => {
     expect(wrapper.html()).toContain('Add your first Todo task')
-  })
+  });
+
+  it('should update done property', async () => {
+    const wrapper = mount(List, {
+      propsData: {
+        list: [{done: false, title: "do this task"}, {done:false,title:"this should not be done"},{done:false,title:"this should also not be done"}]
+      },
+      sync:false
+    });
+    // item exists in the list
+    expect(wrapper.html()).toContain("do this task");
+    wrapper.findAll("span").at(0).trigger('click');
+    await flushPromises(); // wait for the click to resolve
+    expect(wrapper.props().list[0].done).toBe(true);
+  });
 });
+
